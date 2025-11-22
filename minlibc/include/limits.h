4e2938266648e9e5654c2df4211f13ba41f7bb5a
Blake Loring <blake@parsed.uk>
2025-11-22 23:29:55 +0800

    tweaks for bare metal / minilibc

--- minlibc/include/limits.h
+++ minlibc/include/limits.h
@@ -11,6 +11,7 @@
 #define SHRT_MAX        32767
 #define USHRT_MAX       65535
 #define INT_MAX         2147483647
+#define INT_MIN         (-INT_MAX - 1) 
 #define UINT_MAX        4294967295U
 
 #ifdef __x86_64__
